23360ba8eac5da8997386c9c88ea9e4bae825cbe
jzilg <jzg@dasburo.com>
2020-04-13 18:48:50 +0200

    fix that env var works again

--- app/index.js
+++ app/index.js
@@ -1,9 +1,7 @@
+require('dotenv').config()
 const express = require('express')
-const { config } = require('dotenv')
 const routes = require('./router')
 const loggerMiddleware = require('./middleware/loggerMiddleware')
-
-config()
 
 const PORT = 3000
 const app = express()
